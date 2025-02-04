d00dfb001bec1db6f5f89312b52da87c75a2a38b
cl4ireee <cl4ireeee@gmail.com>
2025-02-05 01:32:21 +0700

    Add files via upload

---
+++ public/apiData.js
@@ -0,0 +1,31 @@
+const apiList = [
+    { 
+        category: 'Ai',
+        apis: [
+            { name: 'Ai4Chat', endpoint: 'ai4chat', description: 'Ask with Ai4Chat' },
+            { name: 'Lumin Ai', endpoint: 'luminai', description: 'Talk with luminai.' },
+            { name: 'LilyChanAi', endpoint: 'lilychanai', description: 'Ask with Lilychan.' },
+            { name: 'DeepSeek', endpoint: 'deepseek', description: 'Talk with model Deepseek.' }
+        ]
+    },
+    { 
+        category: 'Anime',
+        apis: [
+            { name: 'Anichin Detail', endpoint: 'anichindetail', description: 'Getting anime details in Anichin.' },
+            { name: 'Anichin Search', endpoint: 'anichinsearch', description: 'Search anime in Anichin.' }
+        ]
+    },
+    { 
+        category: 'Download',
+        apis: [
+            { name: 'Tiktok', endpoint: 'tiktokdl', description: 'Download tiktok video with URL.' }
+        ]
+    },
+    { 
+        category: 'Search Tools',
+        apis: [
+            { name: 'Font', endpoint: 'font', description: 'Search for fonts easily.' },
+            { name: 'SnackVideo', endpoint: 'snackvideos', description: 'Searching video from SnackVideo.' }
+        ]
+    }
+];
