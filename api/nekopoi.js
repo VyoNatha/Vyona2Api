a6e1dc90e6bd49e88a72a09339dbbc82a204c5f9
cl4ireee <cl4ireeee@gmail.com>
2025-02-06 02:22:17 +0700

    Update nekopoi.js

--- api/nekopoi.js
+++ api/nekopoi.js
@@ -1,26 +1,8 @@
 const express = require('express');
-const axios = require('axios');
 const router = express.Router();
 
-// Endpoint untuk mengambil data dari Nekopoi
-router.get('/', async (req, res) => {
-    try {
-        // Mengambil data dari API eksternal
-        const response = await axios.get('https://archive-ui.tanakadomp.biz.id/asupan/nekopoi');
-
-        // Mengubah nama creator menjadi nama Anda
-        const modifiedData = {
-            status: response.data.status,
-            creator: "Claire", // Ganti dengan nama Anda dalam tanda kutip
-            result: response.data.result
-        };
-
-        // Mengembalikan data yang telah dimodifikasi
-        res.json(modifiedData);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Terjadi kesalahan saat mengambil data dari Nekopoi');
-    }
+router.get('/', (req, res) => {
+    res.json({ status: true, message: 'Nekopoi API' });
 });
 
-module.exports = router;
+module.exports = router; // Pastikan ini ada
