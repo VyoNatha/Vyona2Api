d849de48c07bda0b6f1d5dbac243b817447b4e0f
Vyoo <zenpuwuki999@gmail.com>
2025-02-06 17:21:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const path = require('path');
+const app = express();
 const nekopoiRouter = require('./routes/nekopoi');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/nekopoi', nekopoiRouter);
+// Use API routes
+app.use('/api', nekopoiRouter);
 
+// Home route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
