e5c90430119dc9f2611649a0ee2aeb9b4917d2bb
cl4ireee <cl4ireeee@gmail.com>
2025-02-06 10:15:12 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
-// api/index.js
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Import router dari folder api
-const randomanimeRouter = require('./randomanime');
-const randomcosplayRouter = require('./randomcosplay');
-const nekopoiRouter = require('./nekopoi');
-const ai4chatRouter = require('./ai4chat');
-const aianswerRouter = require('./aianswer');
-const anichindetailRouter = require('./anichindetail');
-const anichinsearchRouter = require('./anichinsearch');
-const deepseekRouter = require('./deepseek');
-const fontRouter = require('./font');
-const lilychanaiRouter = require('./lilychanai');
-const luminaiRouter = require('./luminai');
-const lyricsRouter = require('./lyrics');
-const soundcloudsRouter = require('./soundclouds');
-const tiktokdlRouter = require('./tiktokdl');
+// Import router yang telah digabungkan
+const combinedRouter = require('./allapi');
 
 // Middleware untuk parsing JSON
 app.use(express.json());
 
 // Gunakan router
-app.use('/api/randomanime', randomanimeRouter);
-app.use('/api/randomcosplay', randomcosplayRouter);
-app.use('/api/nekopoi', nekopoiRouter);
-app.use('/api/ai4chat', ai4chatRouter);
-app.use('/api/aianswer', aianswerRouter);
-app.use('/api/anichindetail', anichindetailRouter);
-app.use('/api/anichinsearch', anichinsearchRouter);
-app.use('/api/deepseek', deepseekRouter);
-app.use('/api/font', fontRouter);
-app.use('/api/lilychanai', lilychanaiRouter);
-app.use('/api/luminai', luminaiRouter);
-app.use('/api/lyrics', lyricsRouter);
-app.use('/api/soundclouds', soundcloudsRouter);
-app.use('/api/tiktokdl', tiktokdlRouter);
+app.use('/api', combinedRouter); // Menggunakan satu router untuk semua endpoint
 
 // Serve file statis dari folder public
 app.use(express.static(path.join(__dirname, '../public')));
