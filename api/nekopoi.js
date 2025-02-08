128efaf9a7a681389b5c0472b5e4ca2f857b9354
Vyoo <zenpuwuki999@gmail.com>
2025-02-08 23:01:29 +0700

    Update nekopoi.js

--- api/nekopoi.js
+++ api/nekopoi.js
@@ -1,9 +1,7 @@
-const express = require('express');
+// api/neko.js
 const axios = require('axios');
-const router = express.Router();
 
-// Endpoint untuk mengambil data dari Nekopoi
-router.get('/', async (req, res) => {
+export default async function handler(req, res) {
     try {
         // Mengambil data dari API eksternal
         const response = await axios.get('https://archive-ui.tanakadomp.biz.id/asupan/nekopoi');
@@ -11,16 +9,14 @@ router.get('/', async (req, res) => {
         // Mengubah nama creator menjadi nama Anda
         const modifiedData = {
             status: response.data.status,
-            creator: "Claire", // Ganti dengan nama Anda dalam tanda kutip
+            creator: "Claire", // Ganti dengan nama Anda
             result: response.data.result
         };
 
         // Mengembalikan data yang telah dimodifikasi
-        res.json(modifiedData);
+        res.status(200).json(modifiedData);
     } catch (error) {
         console.error(error);
         res.status(500).send('Terjadi kesalahan saat mengambil data dari Nekopoi');
     }
-});
-
-module.exports = router;
+}
